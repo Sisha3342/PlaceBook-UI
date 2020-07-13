6df655025daae79e926ab74e73cf427e057df8e3
spalanevich <alexander.palanevich@gmail.com>
2020-07-13 16:15:10 +0300

    PB-6: Added empty components for routing

---
+++ PlaceBook/src/app/my-maps/my-maps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-maps',
+  templateUrl: './my-maps.component.html',
+  styleUrls: ['./my-maps.component.less']
+})
+export class MyMapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
