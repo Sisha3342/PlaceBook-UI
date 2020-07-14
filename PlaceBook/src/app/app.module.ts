7d9823923f27ff526e7fddfc88a6e5dbb5fafc8c
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-14 15:54:09 +0300

    PB-5: rebuild login with Material UI without validation

--- PlaceBook/src/app/app.module.ts
+++ PlaceBook/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { MyBookingsComponent } from './my-bookings/my-bookings.component';
@@ -39,9 +43,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MatButtonModule,
     MatInputModule,
     MatDividerModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- PlaceBook/src/app/login/login.component.ts
+++ PlaceBook/src/app/login/login.component.ts
@@ -3,25 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
 })
 export class LoginComponent implements OnInit {
   login = '';
   password = '';
 
-  constructor() {
-   }
+  constructor() {}
 
   ngOnInit(): void {
-    this.login = localStorage.getItem('login');
-    this.password = localStorage.getItem('password');
+    // this.login = localStorage.getItem('login');
+    // this.password = localStorage.getItem('password');
   }
 
-  loginUser(event: any): void{
+  loginUser(event: any): void {
     event.preventDefault();
-    const target = event.target;
-    console.log(this.login, this.password);
-    localStorage.setItem('login', this.login);
-    localStorage.setItem('password', this.password);
-   }
+    if (event.target.classList.contains('ng-invalid')) {
+      console.log('form is invalid');
+      return;
+    } else {
+      localStorage.setItem('login', this.login);
+      localStorage.setItem('password', this.password);
+    }
+  }
 }
