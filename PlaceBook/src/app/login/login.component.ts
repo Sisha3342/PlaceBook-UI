757cb280e8447dd715fe608bd7e54c1bd97a6ccb
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-14 18:18:58 +0300

    PB-5: login-form: minimal validation added

--- PlaceBook/src/app/login/login.component.ts
+++ PlaceBook/src/app/login/login.component.ts
@@ -12,14 +12,11 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    // this.login = localStorage.getItem('login');
-    // this.password = localStorage.getItem('password');
-  }
+   }
 
   loginUser(event: any): void {
     event.preventDefault();
     if (event.target.classList.contains('ng-invalid')) {
-      console.log('form is invalid');
       return;
     } else {
       localStorage.setItem('login', this.login);
