6a46ceb3778a6c34354016eab268e2afe7b35005
spalanevich <alexander.palanevich@gmail.com>
2020-07-16 02:08:31 +0300

    PB-11: Added service for getting information about current user

--- PlaceBook/src/app/current-user.service.spec.ts
+++ PlaceBook/src/app/current-user.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { CurrentUserService } from './current-user.service';
+import {CurrentUserService} from './current-user.service';
 
 describe('CurrentUserService', () => {
   let service: CurrentUserService;
--- PlaceBook/src/app/current-user.service.ts
+++ PlaceBook/src/app/current-user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrentUserService {
+  private user: User;
 
-  constructor() { }
+  constructor() {}
+
+  setCurrentUser(): void {
+    this.user.name = localStorage.getItem('name');
+    this.user.password = localStorage.getItem('password');
+    this.user.email = localStorage.getItem('email');
+    this.user.role = localStorage.getItem('role');
+  }
+
+  getCurrentUser(): User {
+    return this.user;
+  }
 }
