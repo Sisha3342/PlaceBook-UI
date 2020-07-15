18313f85f6fac1d559b910f19a72ea0cded25c35
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-15 21:05:49 +0300

    PB-5: create-login-page: added router to myBooking

--- PlaceBook/src/app/login/login.component.ts
+++ PlaceBook/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   password = '';
   warnMessage = 'Invalid credentials';
 
-  constructor(private snackbar: MatSnackBar) {}
+  constructor(private snackbar: MatSnackBar, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -31,8 +32,10 @@ export class LoginComponent implements OnInit {
       });
       return;
     } else {
+      this.snackbar.dismiss();
       localStorage.setItem('login', this.login);
       localStorage.setItem('password', this.password);
+      this.router.navigate(['my_bookings']);
     }
   }
 }
