d326cf5d1bb30b1d01fc1461eca427db495705b2
spalanevich <alexander.palanevich@gmail.com>
2020-07-16 16:33:23 +0300

    PB-11: injected CurrentUserService to sidebar user-card

--- PlaceBook/src/app/sidebar/user-card/user-card.component.ts
+++ PlaceBook/src/app/sidebar/user-card/user-card.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '../../current-user.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
 })
 export class UserCardComponent implements OnInit {
-  userName: string;
-  userImage: string;
+  user: User;
 
-  constructor() { }
+  constructor(private userService: CurrentUserService) {}
 
   ngOnInit(): void {
-    this.userName = 'Sasha';
-    this.userImage = this.userImage = 'https://files.adme.ru/files/news/part_165/1658265/8882015-41015410-1-0-1514194714-1514194724-1500-1-1514194724-650-4561b7ccf5-1514279441.jpg';
+    this.user = this.userService.getCurrentUser();
   }
-
 }
