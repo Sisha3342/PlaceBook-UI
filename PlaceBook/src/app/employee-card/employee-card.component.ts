c53a173a85e17c62de3d86d83ffdc006e88061d8
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-16 20:26:33 +0300

    PB-15: create-emploeeys-page: component added

--- PlaceBook/src/app/employee-card/employee-card.component.ts
+++ PlaceBook/src/app/employee-card/employee-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Employee } from './employee.class';
 
 @Component({
   selector: 'app-employee-card',
   templateUrl: './employee-card.component.html',
-  styleUrls: ['./employee-card.component.scss']
+  styleUrls: ['./employee-card.component.scss'],
 })
 export class EmployeeCardComponent implements OnInit {
+  employeeObject: Employee;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input()
+  set employee(employeeObject: Employee) {
+    this.employeeObject = employeeObject;
   }
 
+  get employee(): Employee {
+    return this.employeeObject;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
