add4b551aa4ca804e3d0e08c00eea8062d893b62
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-22 11:38:04 +0300

    PB-32: create-Booking-details: tooltip delay

--- PlaceBook/src/app/modal-dialog-details/modal-dialog-details.component.ts
+++ PlaceBook/src/app/modal-dialog-details/modal-dialog-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject} from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Employee } from '../employees/employee-card/employee.class';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Booking } from '../models/booking';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-dialog-details',
@@ -11,9 +12,12 @@ import { Booking } from '../models/booking';
 export class ModalDialogDetailsComponent {
   booking: Booking;
 
+  employee: Employee;
+
+  showDelay = new FormControl(500);
+  hideDelay = new FormControl(500);
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: Booking) {
     this.booking = this.data;
   }
-
-  employee: Employee;
 }
