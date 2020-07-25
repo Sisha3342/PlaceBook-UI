1a3cfe3681f6cf0d15f3c8d739dc23cf0c35c064
Sisha3342 <spalanevich@mail.ru>
2020-07-25 23:36:39 +0300

    PB-21: Added getting bookings statistics with get() request

--- PlaceBook/src/app/my-bookings/statistics-box/statistics-box.component.ts
+++ PlaceBook/src/app/my-bookings/statistics-box/statistics-box.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AppMaterialModule } from '../../app-material/app-material.module';
+import { StatisticsService } from './statistics.service';
+import { AuthService } from '../../auth/auth.service';
+import { Statistics } from './statistics';
 
 @Component({
   selector: 'app-statistics-box',
@@ -7,7 +9,25 @@ import { AppMaterialModule } from '../../app-material/app-material.module';
   styleUrls: ['./statistics-box.component.scss'],
 })
 export class StatisticsBoxComponent implements OnInit {
-  constructor() {}
+  total: number;
+  active: number;
+  completed: number;
+  cancelled: number;
 
-  ngOnInit(): void {}
+  constructor(
+    private statisticsService: StatisticsService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.statisticsService
+      .getStatistics(this.authService.getCurrentUser().id)
+      .subscribe((stats: Statistics) => {
+        this.active = stats.active;
+        this.completed = stats.completed;
+        this.cancelled = stats.cancelled;
+
+        this.total = stats.active + stats.completed + this.cancelled;
+      });
+  }
 }
