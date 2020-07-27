3416e12944217e66c4749f5dc2a4d26563dba5a7
Sisha3342 <spalanevich@mail.ru>
2020-07-27 19:28:39 +0300

    PB-44: fixed sending extra change role request on mat-toggle-group init

--- PlaceBook/src/app/employees/employee-card/employee-card.component.ts
+++ PlaceBook/src/app/employees/employee-card/employee-card.component.ts
@@ -26,7 +26,7 @@ export class EmployeeCardComponent implements OnInit {
   ngOnInit(): void {}
 
   changeRole(role: string): void {
-    if (role !== this.role.admin && role !== undefined) {
+    if (role !== this.employeeObject.role) {
       this.employeeService
         .changeRole(this.employeeObject.id, role)
         .subscribe((user: User) => {
