9fc9eda5e0a993731ed4a867fb2d065b83a9df6c
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-27 20:41:34 +0300

    PB-42: create-add-office-modal: css

--- PlaceBook/src/app/editor/add-office-modal/add-office.component.ts
+++ PlaceBook/src/app/editor/add-office-modal/add-office.component.ts
@@ -1,20 +1,6 @@
+import { NgxMaterialTimepickerComponent } from 'ngx-material-timepicker';
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-
-interface Country {
-  value: string;
-  view: string;
-}
-
-interface City {
-  value: string;
-  view: string;
-}
-
-interface Address {
-  value: string;
-  view: string;
-}
 
 @Component({
   selector: 'app-add-office',
@@ -22,23 +8,9 @@ interface Address {
   styleUrls: ['./add-office.component.scss'],
 })
 export class AddOfficeComponent {
-  countries: Country[] = [
-    { value: 'USA', view: 'USA' },
-    { value: 'Ukraine-1', view: 'Ukraine' },
-    { value: 'Belarus-2', view: 'Belarus' },
-  ];
-
-  cities: City[] = [
-    { value: 'Minsk-1', view: 'Minsk' },
-    { value: 'Brest-1', view: 'Brest' },
-    { value: 'Moscow-2', view: 'Moscow' },
-  ];
-
-  addresses: Address[] = [
-    { value: 'Kuprevicha, 3-1', view: 'Kuprevicha, 3' },
-    { value: 'DownStreet-1', view: 'DownStreet' },
-    { value: 'PicadillyStreet-2', view: 'PicadillyStreet' },
-  ];
+  country: string;
+  city: string;
+  address: string;
 
   constructor(public dialogRef: MatDialogRef<AddOfficeComponent>) {}
 
