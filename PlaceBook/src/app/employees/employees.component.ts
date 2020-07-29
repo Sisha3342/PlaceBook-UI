77db197c1b41837481ebd30bd3a8087008298a32
tatyanaN <tatyana.nelipovich@gmail.com>
2020-07-30 00:36:47 +0300

    PB-42: change search with autocomplete

--- PlaceBook/src/app/employees/employees.component.ts
+++ PlaceBook/src/app/employees/employees.component.ts
@@ -1,3 +1,4 @@
+import { SearchService } from './../search/search.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
@@ -9,9 +10,9 @@ import { Observable } from 'rxjs';
 })
 export class EmployeesComponent implements OnInit {
   breakpoint = 0;
-  users: Observable<User[]>;
+  users: User[];
 
-  constructor() {}
+  constructor(private searchService: SearchService) {}
 
   ngOnInit(): void {
     this.breakpoint = Math.floor(window.innerWidth / 450);
@@ -19,5 +20,11 @@ export class EmployeesComponent implements OnInit {
 
   onResize(event): void {
     this.breakpoint = Math.floor(event.target.innerWidth / 450);
+  }
+
+  filterEmployees(searchText: string): void {
+    this.searchService.searchUsers(0, 1000, searchText).subscribe((data) => {
+      this.users = data;
+    });
   }
 }
