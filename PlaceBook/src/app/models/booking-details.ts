c61261eed73287c88a08ea36f12622311113b868
Sisha3342 <spalanevich@mail.ru>
2020-07-31 00:14:17 +0300

    PB-48: fixed displaying booking info for my bookings

---
+++ PlaceBook/src/app/models/booking-details.ts
@@ -0,0 +1,16 @@
+import { OfficeAddress } from './office-address';
+import { BookingMark } from './booking-mark';
+
+export interface BookingDetails {
+  id: number;
+  placeInfo: string;
+  userId: number;
+  userName: string;
+  userSurname: string;
+  photoUrl: string;
+  marks: BookingMark;
+  address: OfficeAddress;
+  timeStart: string;
+  timeEnd: string;
+  status: string;
+}
