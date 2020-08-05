a83a31860a384326bec26c06be8b3ea0d2aed73c
EXADEL\umilanovich <umilanovich@exadel.com>
2020-08-05 15:29:47 +0300

    Move to netlify

---
+++ src/app/employees-bookings/employees-bookings.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeesBookingsComponent } from './employees-bookings.component';
+
+describe('EmployeesBookingsComponent', () => {
+  let component: EmployeesBookingsComponent;
+  let fixture: ComponentFixture<EmployeesBookingsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [EmployeesBookingsComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeesBookingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sidebar/route-button/route-button.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouteButtonComponent } from './route-button.component';
+
+describe('RouteButtonComponent', () => {
+  let component: RouteButtonComponent;
+  let fixture: ComponentFixture<RouteButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [RouteButtonComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RouteButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
